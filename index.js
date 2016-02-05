54056cea70052ff2a95c1f1c19813550ca8e7fd1
James Kyburz <james.kyburz@gmail.com>
2016-02-05 17:20:40 +0100

    inline react.createElement as so small

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-var hyperx = require('hyperx')
 var dragDrop = require('drag-drop')
 
 module.exports = create
 
 function create (react, action) {
-  var hx = hyperx(react.createElement)
   return react.createClass({
     el () {
       var element = this.refs['drag-drop']
@@ -21,6 +19,12 @@ function create (react, action) {
     componentDidMount () { this.update() },
     componentDidUpdate () { this.update() },
     componentWillUnmount () { this.cleanup() },
-    render () { return hx`<div className="drag-drop" ref="drag-drop"></div>` }
+    render () {
+      console.log('calling render')
+      return react.createElement('div', {
+        className: 'drag-drop',
+        ref: 'drag-drop'
+      }, this.props.children)
+    }
   })
 }
