1f98ef4b20c32a1493d313225f682728446c69b9
James Kyburz <james.kyburz@gmail.com>
2017-03-02 10:22:46 +0100

    remove babel, es6 not needed for such a small module

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ module.exports = create
 
 function create (react) {
   return react.createClass({
-    el () {
+    el: function el () {
       var element = this.refs['drag-drop']
       return element instanceof window.Element ? element : element.getDOMNode()
     },
-    update () {
+    update: function update () {
       this.cleanup()
       this.removeDragDrop = dragDrop(this.el(), this.props)
     },
-    cleanup () {
+    cleanup: function cleanup () {
       this.removeDragDrop && this.removeDragDrop()
       this.removeDragDrop = null
     },
-    componentDidMount () { this.update() },
-    componentDidUpdate () { this.update() },
-    componentWillUnmount () { this.cleanup() },
-    render () {
+    componentDidMount: function componentDidMount () { this.update() },
+    componentDidUpdate: function componentDidUpdate () { this.update() },
+    componentWillUnmount: function componentWillUnmount () { this.cleanup() },
+    render: function render () {
       return react.createElement('div', {
         className: 'drag-drop',
         ref: 'drag-drop'
