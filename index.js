a477a27e7c2bb6e5a6243347cde6273bf16e0508
James Kyburz <james.kyburz@gmail.com>
2017-03-02 10:25:39 +0100

    ref refactor

--- index.js
+++ index.js
@@ -4,13 +4,9 @@ module.exports = create
 
 function create (react) {
   return react.createClass({
-    el: function el () {
-      var element = this.refs['drag-drop']
-      return element instanceof window.Element ? element : element.getDOMNode()
-    },
     update: function update () {
       this.cleanup()
-      this.removeDragDrop = dragDrop(this.el(), this.props)
+      this.removeDragDrop = dragDrop(this.refs['drag-drop'], this.props)
     },
     cleanup: function cleanup () {
       this.removeDragDrop && this.removeDragDrop()
@@ -22,7 +18,7 @@ function create (react) {
     render: function render () {
       return react.createElement('div', {
         className: 'drag-drop',
-        ref: 'drag-drop'
+        ref: function ref (el) { this.refs['drag-drop'] = el }
       }, this.props.children)
     }
   })
